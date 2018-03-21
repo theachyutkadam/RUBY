aed3167272e03ffced679f60450b8ad9ebbec13f
AK <theachyutkadam@gmail.com>
2018-03-21 13:20:13 +0530

    show movie list

--- movie.rb
+++ movie.rb
@@ -1,9 +1,11 @@
-puts "Movie List"
-# movie_name = hash]("IRONMAN", "THOR", "HULK", "AVENGERS", "ANTMAN")
+class Movie
+  def movie_list
+    puts "Movie List"
+    movie_name = ['IRONMAN', 'THOR', 'HULK', 'AVENGERS', 'ANTMAN']
+    movie_name.each { |movie| puts movie }
+  end
 
+end
 
-puts "IRONMAN"
-puts "THOR"
-puts "HULK"
-puts "AVENGERS"
-puts "ANTMAN"
+ movie_list = Movie.new
+ movie_list.movie_list
