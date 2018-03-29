16fd3130ff9295d6a8b148848467555cc2043ef8
AK <theachyutkadam@gmail.com>
2018-03-29 12:51:06 +0530

    accept input form user

--- movie.rb
+++ movie.rb
@@ -1,4 +1,5 @@
 class Movie
+  
   def movie_list
     puts "Movie List"
     movie_name = ['IRONMAN', 'THOR', 'HULK', 'AVENGERS', 'ANTMAN']
@@ -7,5 +8,9 @@ class Movie
 
 end
 
- movie_list = Movie.new
- movie_list.movie_list
+  movie_list = Movie.new
+  movie_list.movie_list
+  puts "select movie using number"
+  # movie_name = movie_name.chomp
+  movie_name = gets
+  print "you select" + movie_name + "movie"
