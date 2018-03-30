8dfb5b3aa9f8f1a66da245e92c8cb6598d70ce88
AK <theachyutkadam@gmail.com>
2018-03-30 22:19:18 +0530

    show movie list

--- movie.rb
+++ movie.rb
@@ -1,16 +1,22 @@
 class Movie
   
   def movie_list
-    puts "Movie List"
-    movie_name = ['IRONMAN', 'THOR', 'HULK', 'AVENGERS', 'ANTMAN']
-    movie_name.each { |movie| puts movie }
+    movie_name = ["IRONMAN", "THOR", "HULK", "AVENGERS", "ANTMAN"]
+    # movie_name.each { |movie| puts movie.size }
+    movie_name.each {|x| 
+    	puts x
+    }
+    puts ""
+    print "Select Your Movie Using Number : "
+    movie_number = gets
+    movie = movie_number string.to_s
+    print movie_name[movie]
+    return movie_number
+
   end
 
 end
 
   movie_list = Movie.new
-  movie_list.movie_list
-  puts "select movie using number"
-  # movie_name = movie_name.chomp
-  movie_name = gets
-  print "you select" + movie_name + "movie"
+  movie_number = movie_list.movie_list
+  print "you select : " + movie_number
